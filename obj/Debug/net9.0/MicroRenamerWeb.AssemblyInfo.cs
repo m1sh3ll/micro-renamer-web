251507034d14ee18186c6635f46b2ae1364233a1
michelle nesbitt <mn218@nerdymail.com>
2026-04-14 18:12:39 -0400

    mn

--- obj/Debug/net9.0/MicroRenamerWeb.AssemblyInfo.cs
+++ obj/Debug/net9.0/MicroRenamerWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MicroRenamerWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8eb9b683ae09464ef7afbe5eae1f6b7fbfdb72fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+398fe140c1295f30a89db8a29a3d5f4210ca2987")]
 [assembly: System.Reflection.AssemblyProductAttribute("MicroRenamerWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MicroRenamerWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
